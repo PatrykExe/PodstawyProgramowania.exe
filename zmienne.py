27038c2228a08ee8f67cd7dd95ee7161db567fa7
Patryk Krajewski inf <Patkraj50@edu.gdansk.plgit config --global user.email Patkraj50@edu.gdansk.plgit config --global user.email Patkraj50@edu.gdansk.pl>
2025-10-07 14:01:43 +0200

    lekcja na temat zmiennych

---
+++ zmienne.py
@@ -0,0 +1,32 @@
+#Co tu sie od... dzieje
+#1) rezerwujemy kawałek pamięci
+#2) umieszczamy w tej pamięci liczbę 45
+#3) nadajemy temu kawałkowi pamięci liczba całkowita
+
+liczba_calkowita = 45
+print(type(liczba_calkowita))
+
+liczba_z_przecinkiem = 2.65
+print(type(liczba_z_przecinkiem))
+
+'''
+to są pozostałe
+typy zmiennych
+również są świetne
+'''
+
+napis = "67.informatyka.67"
+print(type(napis))
+
+wartosc_nielogiczna = True
+print(type(wartosc_nielogiczna))
+
+lista = [5, 'x', [5, 7], [[4, 6], 9], True, 'nigger']
+print(lista)
+
+zbior = {5, 6, 7, 8, 9, 5}
+print(zbior)
+print(type(zbior))
+
+slownik = {'nigger': 'wyśmiewisko, kierowane do ludności czarnoskórej'}
+print(type(slownik))
