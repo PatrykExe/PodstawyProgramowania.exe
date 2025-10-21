5ba216bd5437b0f9dba1adb17b59e3b0671b3a9c
Patryk Krajewski inf <Patkraj50@edu.gdansk.plgit config --global user.email Patkraj50@edu.gdansk.plgit config --global user.email Patkraj50@edu.gdansk.pl>
2025-10-21 13:42:26 +0200

    .

--- operatory_logiczne_i_arytmetyczne.py
+++ operatory_logiczne_i_arytmetyczne.py
@@ -4,9 +4,37 @@
 print(2 == 3 and 3 > 1)
 
 #Alternatywa
-login = 'nissan_skyline'
-haslo = 'GT-R 34'
-print(login == 'nissan' or haslo == 'GT-R 34')
+login = 'Pk'
+haslo = 'KP'
+print(login == 'pK' or haslo == 'KP')
 
 #zaprzeczenie
-print(not(login == 'nissan' or haslo == 'GT-R 34'))
+print(not(login == 'Pk' or haslo == 'kP'))
+
+#Alternatywa Wykluczająca
+fryzjer = True
+murarz = False
+
+print(fryzjer == True ^ murarz == True)
+
+#Priorytety operatorów logicznych
+print(2 == 3 and 3 < 1 or 2 < 6)
+
+#operatory standardowe
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(2 / 4)
+
+#Potęgowanie
+print(2 ** 4)
+
+# Pierwiastki
+print(36 ** 0.5)
+print(125 ** (1/3))
+
+#dzielernie całkowite (div) - zaokrąglenie zawsze w dół do liczby całkowitej
+print(22 // 5)
+
+#reszta z dzielenia
+print(12 % 5)
