d187dae15b6d0c9e1a8248efe50806ec1f4f1e48
Patryk Krajewski inf <Patkraj50@edu.gdansk.plgit config --global user.email Patkraj50@edu.gdansk.plgit config --global user.email Patkraj50@edu.gdansk.pl>
2025-10-21 13:13:03 +0200

    ...

---
+++ operatory_logiczne_i_arytmetyczne.py
@@ -0,0 +1,12 @@
+#symbole logiczne
+
+#koniunkcja
+print(2 == 3 and 3 > 1)
+
+#Alternatywa
+login = 'nissan_skyline'
+haslo = 'GT-R 34'
+print(login == 'nissan' or haslo == 'GT-R 34')
+
+#zaprzeczenie
+print(not(login == 'nissan' or haslo == 'GT-R 34'))
