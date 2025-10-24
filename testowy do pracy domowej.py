e54bf356a1e933a2fc646e5b7ab06a437bfee126
PatrykExe <krajewski.patryk11@gmail.com>
2025-10-24 17:46:48 +0200

    Praca domowa 28.10.2025

--- testowy do pracy domowej.py
+++ testowy do pracy domowej.py
@@ -1,16 +1,22 @@
 from math import sqrt
+a = float(input('Podaj liczbę rzeczywistą, która jest inna niż zero: '))
+b = float(input('Podaj liczbę rzeczywistą: '))
+c = float(input('Podaj liczbę rzeczywistą: '))
 
-a = float(input('Podaj liczbę a, która nie jest równa 0: '))
-b = float(input('Podaj liczbę b: '))
-c = float(input('Podaj liczbę c: '))
 d = b ** 2 - 4 * a * c
+if a == 0:
+    print('czytać nie umisz?')
 
-if d > 0:
-    x1 = (-b + sqrt(d)) / (2 * a)
-    x2 = (-b - sqrt(d)) / (2 * a)
-    print(f'x1 = {x2}, x2 = {x2}')
+elif d > 0:
+    x1 = (-b - sqrt(d)) / (2 * a)
+    x2 = (-b + sqrt(d)) / (2 * a)
+    print(f'x1 = {x1}')
+    print(f'x2 = {x2}')
+
 elif d == 0:
     x = (-b) / (2 * a)
-    print(f'x1 == x2 = {x}')
+    print(f'x1 = x2')
+    print(f'x = {x}')
+
 else:
-    print('nie ma rozwiązania')
+    print('Rozwiązanie nie istnieje ty inwestycyjna wtopo twojej linii genetycznej')
