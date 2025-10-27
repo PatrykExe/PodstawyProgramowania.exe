597f0cbb35646c25996601ccb8253e99d22034cf
PatrykExe <krajewski.patryk11@gmail.com>
2025-10-27 17:11:58 +0100

    Edycja pliku: Mój projekt

--- Mój projekt.py
+++ Mój projekt.py
@@ -1,44 +1,36 @@
 import random
 
-a = int(input('Podaj liczbę od 1 do 9: '))
-b = int(random.randrange(1, 10))
+nagroda = ['10zł', '20zł', '15 zł', '25zł', '30zł', '50zł' 'monster ultra white', 'monster ultra paradise']
+kara = ['2zł', '3zł', '5zł', '10zł' 'cały maraton sprintem (udokumentowane)']
 
-nagroda = ['rozgrzeszenie', 'spotify premium na rok']
-kara = ['1v1 z niedzwiedziem', 'cały maraton sprintem (udokumentowane)', 'multiplą do okoła świata']
+while True:  # Jest to pętla, która będzie aktywna do momentu zgadnięcia liczby przez użytkownika
+    a = int(input('Podaj liczbę od 1 do 9: '))
+    b = random.randrange(1, 10)
 
-decyzja = input('Jesteś pewien co do liczby? (tak/nie): ')
+    decyzja = input('Jesteś pewien co do liczby? (tak/nie): ').lower()
 
-if decyzja == "tak":
-    if a == b:
-        print(f'Brawo, wygrałeś {random.choice(nagroda)}')
+    if decyzja == "tak":
+        if a == b:
+            print(f'Brawo, wygrałeś {random.choice(nagroda)}!')
+            break  # kończy pętlę, bo zgadł
+        elif b == (a + 1) or b == (a - 1):
+            print(f'No, blisko było, ale jednak żeś nie trafił: {random.choice(kara)}')
+            print(f'Liczbą była cyfra {b}')
+        else:
+            print(f'Przegrałeś, a karą jest {random.choice(kara)}')
+            print(f'Liczbą była cyfra {b}')
 
-    elif b == (a + 1):
-        print(f'No, Blisko było ale jednak przewaliłeś, więc {random.choice(kara)}')
-        print(f'Liczbą była cyfra {b}')
-
-    elif b == (a - 1):
-        print(f'No, Blisko było ale jednak przewaliłeś, więc {random.choice(kara)}')
-        print(f'Liczbą była cyfra {b}')
-
-    else:
-        print(f'przegrałeś, a karą jest {random.choice(kara)}')
-        print(f'Liczbą była cyfra {b}')
-
-elif decyzja == "nie":
-    c = int(input('Podaj liczbę, ale następnej szansy nie ma: '))
-
-    if c == b:
-        print(f'Brawo, wygrałeś {random.choice(nagroda)}')
-
-    elif b == (c + 1):
-        print(f'No, Blisko było ale jednak przewaliłeś, więc {random.choice(kara)}')
-        print(f'Liczbą była cyfra {b}')
-
-    elif b == (c - 1):
-        print(f'No, Blisko było ale jednak przewaliłeś, więc {random.choice(kara)}')
-        print(f'Liczbą była cyfra {b}')
+    elif decyzja == "nie":
+        c = int(input('Podaj nową liczbę (następnej szansy nie ma!): '))
+        if c == b:
+            print(f'Brawo, wygrałeś {random.choice(nagroda)}!')
+            break
+        elif b == (c + 1) or b == (c - 1):
+            print(f'No, blisko było, ale jednak żeś nie trafił: {random.choice(kara)}')
+            print(f'Liczbą była cyfra {b}')
+        else:
+            print(f'Przegrałeś, a karą jest {random.choice(kara)}')
+            print(f'Liczbą była cyfra {b}')
 
     else:
-        print(f'przegrałeś, a karą jest {random.choice(kara)}')
-        print(f'Liczbą była cyfra {b}')
-
+        print('Nie rozumiesz? Odpowiedz "tak" lub "nie".')
