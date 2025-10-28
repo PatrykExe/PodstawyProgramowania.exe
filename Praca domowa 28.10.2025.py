7e2bc7c004fbbdfbc30c7b376a8e03a5f4f7c168
Patryk Krajewski inf <Patkraj50@edu.gdansk.plgit config --global user.email Patkraj50@edu.gdansk.plgit config --global user.email Patkraj50@edu.gdansk.pl>
2025-10-28 14:10:00 +0100

    Delta +

--- Praca domowa 28.10.2025.py
+++ Praca domowa 28.10.2025.py
@@ -1,4 +1,5 @@
 #ZADANIE 13:
+from traceback import print_tb
 
 test_kompetencji_jezykowych = int(input('Podaj swój wynik z testu kompetencji. Wynik podaj w procentach: '))
 ocena_ze_swiadectwa = int(input('Podaj swoją ocenę ze świadectwa: '))
@@ -18,17 +19,30 @@ c = float(input('Podaj następną liczbę rzeczywistą: '))
 
 d = b ** 2 - 4 * a * c
 # d == delta (mój skrót myślowy)
-
-if d > 0:
-    x1 = (-b + sqrt(d)) / (2 * a)
-    x2 = (-b - sqrt(d)) / (2 * a)
-    print(f'x1 = {x1}')
-    print(f'x2 = {x2}')
-    #rozbiłem na dwa print(), aby było czytelniejsze
-elif d == 0:
-    x = (-b) / (2 * a)
-    print(f'x = {x}')
-else:
-    print('nie ma rozwiązań')
+if b == 0 and c == 0:
+    print(f'wynik= 0')
+if b == 0 and c != 0:
+    if -c/a > 0:
+        x1 = (-c/a) ** 0.5
+        x2 = -((-c/a) ** 0.5)
+        print(f'x1 = {x1} v x2 = {x2}')
+    if -c/a < 0:
+        print('brak rozwiązań')
+if c == 0 and b != 0:
+    x1 = 0
+    x2 =(-b)/a
+    print(f'x1 = {x1} v x2 = {x2}')
+if b != 0 and c != 0:
+    if d > 0:
+        x1 = (-b + sqrt(d)) / (2 * a)
+        x2 = (-b - sqrt(d)) / (2 * a)
+        print(f'x1 = {x1}')
+        print(f'x2 = {x2}')
+        #rozbiłem na dwa print(), aby było czytelniejsze
+    elif d == 0:
+        x = (-b) / (2 * a)
+        print(f'x = {x}')
+    else:
+        print('nie ma rozwiązań')
 
 
