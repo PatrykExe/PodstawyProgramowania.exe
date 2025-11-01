c100effbe6a9a815af7813b28822d38983cff748
PatrykExe <krajewski.patryk11@gmail.com>
2025-11-01 15:46:53 +0100

    Mój projekt

--- Mój projekt.py
+++ Mój projekt.py
@@ -3,6 +3,8 @@ import random
 nagroda = ['10zł', '20zł', '15 zł', '25zł', '30zł', '50zł' 'monster ultra white', 'monster ultra paradise']
 kara = ['2zł', '3zł', '5zł', '10zł', 'cały maraton sprintem (udokumentowane)']
 
+otrzymane_kary = []  #Lista od otrzymanych kar, które potem zostaną wyświetlone (skrypt od 40 linijki)
+
 while True:  # Jest to pętla, która będzie aktywna do momentu zgadnięcia liczby przez użytkownika
     a = int(input('Podaj liczbę od 1 do 9: '))
     b = random.randrange(1, 10)
@@ -12,7 +14,7 @@ while True:  # Jest to pętla, która będzie aktywna do momentu zgadnięcia lic
     if decyzja == "tak":
         if a == b:
             print(f'Brawo, wygrałeś {random.choice(nagroda)}!')
-            break  # kończy pętlę, bo zgadł
+            break  # kończy pętlę
         elif b == (a + 1) or b == (a - 1):
             print(f'No, blisko było, ale jednak żeś nie trafił: {random.choice(kara)}')
             print(f'Liczbą była cyfra {b}')
@@ -34,3 +36,11 @@ while True:  # Jest to pętla, która będzie aktywna do momentu zgadnięcia lic
 
     else:
         print('Po Polskiemu nie rozumiesz? Odpowiedzi są dwie: "tak" lub "nie".')
+
+if otrzymane_kary:
+    print("\n Twoje kary: ")
+    for idx, k in enumerate():
+        print(f"{idx}. {k}")  #idx = index; k = poszczególna kara
+        print(f"No cóż, otrzymałeś razem {otrzymane_kary} kar(y)")
+else:
+    print("\nSzacun, pierwsza próba!!")
--- zmienna typu string.py
+++ zmienna typu string.py
@@ -1,16 +1,16 @@
 napis = 'kuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuurkuma'
 #I: Fragment tekstu:
 #1) Wycinanie od ... do\
-print(napis[34:39]) # czyli tak naprawdę od 2 do 4
+print(napis[34:39]) # czyli tak naprawdę od 34 do 38
 
 #2) Wycinanie od .. do (co ileś)
-print(napis[0:40:2])
+print(napis[0:40:2]) #(trzecia wartość - co 2 litera jest wyświetlana
 
 #3) wycinanie od początku
-print(napis[:1])
+print(napis[:1]) #Zostawienie pustego jest równoważne z 0
 
 #4) wycinanie do końca
-print(napis[2:])
+print(napis[2:]) #usuwa pierwsze 2 litery
 
 #5) czytanie od końca
 print(napis[::])
@@ -22,10 +22,10 @@ print(napis[::-1])
 if 'a' in napis:
     print("należy")
 else:
-    print("nienależy")
+    print("nima")
 
 #III: Łączenie napisów (konkatenacja)
-napis2 = napis + 'jestnajlepsza'
+napis2 = napis + 'jestnajlepsza' #podajesz zdefiniowane słowo i dodajesz to co chcesz dodać
 print(napis2)
 
 #IV: Funkcje zmiennych typu string
@@ -49,26 +49,28 @@ if napis4.find('xyz') != -1:
     print('xyz jest w napisie')
 else:
     print('nima')
+#"!= -1" oznacza, że cośnie jest równe temu, że nie znaleziono, czyli poprostu znaleziono
 
 #2) Podział tekstu na fragmenty
-"""piec_liczb = input('Podaj pięć liczb. Odziel je przecinkiem')
+
+piec_liczb = input('Podaj pięć liczb. Odziel je przecinkiem')
 piec_liczb_po_przedziale = piec_liczb.split(',')
 print(piec_liczb_po_przedziale)
 trzecia_liczba = int(piec_liczb_po_przedziale[2])
-print(trzecia_liczba + 33)"""
+print(trzecia_liczba + 33)
 
 #3) Łączenie napisów
 
 lista_napisów = ['Windows', 'XP', 'został', 'stworzony', 'z', 'pasją']
-cale_zdanie = ' '.join(lista_napisów)
+cale_zdanie = ' '.join(lista_napisów) #join() umożliwia podanie 'separatora', którym będą oddzielone elementy listy
 print(cale_zdanie)
 
 lista_napisow2 = ['Windows', 'XP', 'został', 'stworzony', 'z', 'pasją']
-cale_zdanie2 = '\n'.join(lista_napisow2)
+cale_zdanie2 = '\n'.join(lista_napisow2) #\n odpowiada za przeniesienie kolejnych elementów do innych następnych linijek
 print(cale_zdanie2)
 
 #3) zliczanie danego znaku w tekscie
 
 napis5 = 'prawdopodobieństwo'
-ile_razy_o = napis5.count('o')
+ile_razy_o = napis5.count('o') #count() odpowiada za zliczenie elementów słowa lub listy, które zostały wybrane (np. a lub 12)
 print(ile_razy_o)
