242d1e6a78e99a9b20bd8bbf137f4c7a23e8c96d
PatrykExe <krajewski.patryk11@gmail.com>
2025-11-20 17:42:36 +0100

    Mój Projekt

--- Mój projekt.py
+++ Mój projekt.py
@@ -1,45 +1,68 @@
 import random
 
-nagroda = ['10zł', '20zł', '15 zł', '25zł', '30zł', '50zł' 'monster ultra white', 'monster ultra Ruby red']
-kara = ['2zł', '3zł', '5zł', '10zł', 'cały maraton sprintem (udokumentowane)']
+nagroda = [10, 20, 15, 25, 30, 50]
+kara = [2, 3, 5, 10]
 
+saldo = 0
 
-
-while True:  # Jest to pętla, która będzie aktywna do momentu zgadnięcia liczby przez użytkownika
+while True:
     a = int(input('Podaj liczbę od 1 do 9: '))
-    b = random.randrange(1, 10)
-    otrzymane_kary = 0  #Lista od otrzymanych kar, które potem zostaną wyświetlone (skrypt od 40 linijki)
+    b = random.randint(1, 9)
+
     decyzja = input('Jesteś pewien co do liczby? (tak/nie): ').lower()
 
     if decyzja == "tak":
+
         if a == b:
-            print(f'Brawo, wygrałeś {random.choice(nagroda)}!')
-            break  # kończy pętlę
-        elif b == (a + 1) or b == (a - 1):
-            print(f'No, blisko było, ale jednak żeś nie trafił: {random.choice(kara)}')
-            print(f'Liczbą była cyfra {b}')
+            # wygrana
+            if random.random() < 0.5:
+                wygrana = random.choice(nagroda)
+                saldo += wygrana
+                print(f"Brawo! Wygrałeś {wygrana} zł!")
+            break
+
         else:
-            print(f'Przegrałeś, a karą jest {random.choice(kara)}')
-            print(f'Liczbą była cyfra {b}')
+            # przegrana
+            wylosowana_kara = random.choice(kara)
+            saldo -= wylosowana_kara
+
+            if b == a + 1 or b == a - 1:
+                print(f"Blisko! Ale nie trafiłeś. Kara: {wylosowana_kara} zł")
+            else:
+                print(f"nie tym razem! Twoja kara to {wylosowana_kara} zł")
+
+            print(f"Prawidłowa liczba to {b}")
+            continue
 
     elif decyzja == "nie":
-        c = int(input('Podaj nową liczbę (następnej szansy nie ma!): '))
-        if c == b:
-            print(f'Brawo, wygrałeś {random.choice(nagroda)}!')
+        a = int(input("Podaj nową liczbę (ostatnia szansa!): "))
+
+        if a == b:
+            if random.random() < 0.5:
+                wygrana = random.choice(nagroda)
+                saldo += wygrana
+                print(f"Brawo! Wygrałeś {wygrana} zł!")
             break
-        elif b == (c + 1) or b == (c - 1):
-            print(f'No, blisko było, ale jednak żeś nie trafił: {random.choice(kara)}')
-            print(f'Liczbą była cyfra {b}')
+
         else:
-            print(f'Przegrałeś, a karą jest {random.choice(kara)}')
-            print(f'Liczbą była cyfra {b}')
+            wylosowana_kara = random.choice(kara)
+            saldo -= wylosowana_kara
 
+            if b == a + 1 or b == a - 1:
+                print(f"Blisko! Ale nie trafiłeś. Kara: {wylosowana_kara} zł")
+            else:
+                print(f"Źle! Twoja kara to {wylosowana_kara} zł")
+
+            print(f"Prawidłowa liczba to {b}")
+            continue
     else:
-        print('Po Polskiemu nie rozumiesz? Odpowiedzi są dwie: "tak" lub "nie".')
+        print('Odpowiedzi są tylko dwie: "tak" lub "nie".')
+        continue
 
-if otrzymane_kary >= 1:
-    if a != b and c != b:
-         otrzymane_kary = otrzymane_kary + random.choice(kara)
-         print(f"No cóż, otrzymałeś razem {otrzymane_kary} kar(y)")
-    elif a == b or c == b:
-        print('Zgadłeś za pierwszym razem.')
+print("\n       PODSUMOWANIE")
+if saldo > 0:
+    print(f"Zyskujesz: +{saldo} zł")
+elif saldo < 0:
+    print(f"tracisz {saldo} zł")
+else:
+    print("nic nie wygrałeś, ani nie przegrałeś.")
