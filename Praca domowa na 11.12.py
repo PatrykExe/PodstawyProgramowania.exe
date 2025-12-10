9b2d5a122821be68f6e9c41c283aa1cabc00e73b
PatrykExe <krajewski.patryk11@gmail.com>
2025-12-10 17:16:14 +0100

    praca domowa

---
+++ Praca domowa na 11.12.py
@@ -0,0 +1,17 @@
+'''Zadanie 3:
+    Efektem takiego kodu będzie wypisanie
+sumy liczb parzystych. Mam na myśli to,
+że "if n % 2 == 1: " oznacza, że dzielenie
+liczby n przez 2 daje resztę 1, czyli jeżeli
+n jest nieparzyste, to go nie dodaje do sumy,
+natomiast parzyste doda. Ostateczny wynik to 90
+'''
+
+#Zadanie 4:
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
+print('--------------')
+for i in range(10, 0, -1):
+    print(i)
